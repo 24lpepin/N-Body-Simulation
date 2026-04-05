b1eb4a9f430b4f5981f5e65eccbcb982015ee0e4
Luke Pepin <lpepin1123@gmail.com>
2026-04-05 10:46:25 -0400

    Some comments to object class

--- object.cpp
+++ object.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <vector>
-
 #include "geometry.h"
 #include "object.h"
 #include "const.h"
@@ -9,10 +8,14 @@ Object::Object(Vector2D position, Vector2D velocity, Vector2D acceleration, floa
     velocity(velocity), acceleration(acceleration), mass(mass) {}
 
 std::ostream& operator<<(std::ostream& os, const Object& o) {
-    return os << "position: " << o.position << std::endl << "velocity: " << o.velocity << std::endl << 
-        "acceleration: " << o.acceleration;
+    return os << "position: " << o.position << std::endl 
+              << "velocity: " << o.velocity << std::endl 
+              << "acceleration: " << o.acceleration;
 }
 
+/**
+ * Uses standard implementation of velocity Verlet algorithm.
+ */
 void Object::step(const std::vector<Object>& objects, float dt) {
     velocity = velocity + 0.5 * acceleration * dt;
     position = position + velocity * dt;
@@ -20,10 +23,13 @@ void Object::step(const std::vector<Object>& objects, float dt) {
     velocity = velocity + 0.5 * acceleration * dt;
 }
 
+/**
+ * Updates the acceleration by computing the force due to each object in the simulation.
+ */
 void Object::update_acceleration(const std::vector<Object>& objects) {
     acceleration = Vector2D(0,0);
     for (const Object& object : objects) {
-        if (&object == this) continue; 
+        if (&object == this) continue; // Skip the calling object
 
         Vector2D r = object.position - position;
         float r2 = r.magnitude() * r.magnitude();
--- object.h
+++ object.h
@@ -1,9 +1,12 @@
 #pragma once
 #include <iostream>
 #include <vector>
-
 #include "geometry.h"
 
+/**
+ * The Object class stores relevant information about a simulation object and provides methods to 
+ * update said information.
+ */
 class Object {
     public:
         Vector2D position;
@@ -15,7 +18,15 @@ class Object {
 
         friend std::ostream& operator<<(std::ostream& os, const Object& o);
 
+        /**
+         * Takes in a list of simulation objects and a timestep dt and updates the object's 
+         * position, velocity, and acceleration.
+         */
         void step(const std::vector<Object>& objects, float dt);
 
+    private:
+        /**
+         * Updates the object's acceleration based on the position of the given list of objects.
+         */
         void update_acceleration(const std::vector<Object>& objects);
 };
