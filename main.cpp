e557ed29f36db0c6e6c88fc8a4aab4585ec7e5e4
Luke <lpepin1123@gmail.com>
2026-04-10 14:01:16 -0400

    SFML works

--- main.cpp
+++ main.cpp
@@ -5,18 +5,44 @@
 #include "geometry.h"
 #include "object.h"
 
-int main() {
-    std::vector<Object> objects;
+#include <./SFML/Graphics.hpp>
 
-    Vector2D x(0,0);
-    Vector2D v(1,1);
-    Vector2D a(0,0);
-    double m = 1;
-    Object o(x,v,a,m);
+int main()
+{
+    std::cout << "running";
+    sf::RenderWindow window(sf::VideoMode({200, 200}), "SFML works!");
+    window.setPosition({100,100});
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
 
-    objects.push_back(o);
-    std::cout << o << std::endl;
-    o.step(objects, 0.05);
-    std::cout << o << std::endl;
+    while (window.isOpen())
+    {
+        while (const std::optional event = window.pollEvent())
+        {
+            if (event->is<sf::Event::Closed>())
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
     return 0;
 }
+
+// int main() {
+//     std::vector<Object> objects;
+
+//     Vector2D x(0,0);
+//     Vector2D v(1,1);
+//     Vector2D a(0,0);
+//     double m = 1;
+//     Object o(x,v,a,m);
+
+//     objects.push_back(o);
+//     std::cout << o << std::endl;
+//     o.step(objects, 0.05);
+//     std::cout << o << std::endl;
+//     return 0;
+// }
